647bcdfafb5e58c760053e35dc14cf7e5bcb099e
bremxx <petr.kirienko@gmail.com>
2021-02-17 19:54:09 +0300

    логика загрузки превью видео внутри карточки по наведению мыши

--- src/components/film-card/film-card.jsx
+++ src/components/film-card/film-card.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {filmMockPropTypes} from '../../prop-types';
+import PlayerPreview from '../player-preview/player-preview';
 
-const FilmCard = ({id, name, previewImage}) => {
+const FilmCard = ({id, name, previewImage, previewVideoLink}) => {
+  const [startPlayer, setStartPlayer] = useState(false);
+
   return (
-    <article className="small-movie-card catalog__movies-card">
+    <article
+      className="small-movie-card catalog__movies-card"
+      onMouseOver={() => {
+        setStartPlayer(true);
+      }}
+      onMouseLeave={() => {
+        setStartPlayer(false);
+      }}
+    >
       <div className="small-movie-card__image">
-        <img src={previewImage} alt={name} width="280" height="175" />
+        {
+          startPlayer
+            ? <PlayerPreview
+              previewVideoLink={previewVideoLink}
+              previewImage={previewImage}
+            />
+            : <img src={previewImage} alt={name} width="280" height="175" />
+        }
       </div>
       <h3 className="small-movie-card__title">
-        <Link className="small-movie-card__link" to={`/films/${id}`}>{name}</Link>
+        <Link
+          className="small-movie-card__link"
+          to={`/films/${id}`}
+          onClick={() => setStartPlayer(false)}
+        >{name}
+        </Link>
       </h3>
     </article>
   );
