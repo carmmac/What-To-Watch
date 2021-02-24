3339e69017ac4c4c4673adda34e6d8c9c26224b7
bremxx <petr.kirienko@gmail.com>
2021-02-24 19:53:39 +0300

    черновик редьюсера

---
+++ src/store/reducer.js
@@ -0,0 +1,29 @@
+import films from "../mock/films-mock";
+import {getRandomNum} from "../utils";
+import ActionType from "./action";
+
+const initialState = {
+  promoFilm: films[Math.floor(getRandomNum(0, films.length - 1))],
+  genre: `Drama`,
+  films,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.GENRE_SELECT:
+      return {
+        ...state,
+        genre: action.payload
+      };
+    case ActionType.GET_FILMS:
+      return {
+        ...state,
+        films: action.payload
+      };
+  }
+  return state;
+};
+
+export {
+  reducer,
+};
