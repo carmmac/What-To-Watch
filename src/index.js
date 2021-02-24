7a914785258acff25c2f857ffe26bd9ba9b217f6
bremxx <petr.kirienko@gmail.com>
2021-02-24 19:59:58 +0300

    подключение и инициализация хранилища в точке входа

--- src/index.js
+++ src/index.js
@@ -5,15 +5,23 @@ import films from './mock/films-mock.js';
 import reviews from './mock/reviews-mock.js';
 import {getRandomNum} from './utils';
 import {FilmPageTab} from './const.js';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from './store/reducer';
+
+const store = createStore(reducer, composeWithDevTools());
 
 const promoFilm = films[Math.floor(getRandomNum(0, films.length - 1))];
 
 ReactDOM.render(
-    <App
-      promoFilm={promoFilm}
-      films={films}
-      reviews={reviews}
-      filmPageTab={FilmPageTab}
-    />,
+    <Provider store={store}>
+      <App
+        promoFilm={promoFilm}
+        films={films}
+        reviews={reviews}
+        filmPageTab={FilmPageTab}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
