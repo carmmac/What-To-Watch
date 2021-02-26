c72f7ab244e11ae79947b431b32682eda3d482cc
bremxx <petr.kirienko@gmail.com>
2021-02-26 19:18:08 +0300

    почистил пропсы

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import films from './mock/films-mock.js';
 import reviews from './mock/reviews-mock.js';
-import {getRandomNum} from './utils';
-import {FilmPageTab} from './const.js';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
@@ -12,15 +10,11 @@ import {reducer} from './store/reducer';
 
 const store = createStore(reducer, composeWithDevTools());
 
-const promoFilm = films[Math.floor(getRandomNum(0, films.length - 1))];
-
 ReactDOM.render(
     <Provider store={store}>
       <App
-        promoFilm={promoFilm}
         films={films}
         reviews={reviews}
-        filmPageTab={FilmPageTab}
       />
     </Provider>,
     document.querySelector(`#root`)
