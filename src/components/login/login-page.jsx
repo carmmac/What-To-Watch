c7d0949509f9c24f205cc4c195691e882d668f1a
bremxx <petr.kirienko@gmail.com>
2021-03-08 21:02:30 +0300

    определение доступа к странице авторизации по значению статуса пользователя

--- src/components/login/login-page.jsx
+++ src/components/login/login-page.jsx
@@ -1,12 +1,13 @@
 import React, {useRef} from 'react';
 import PropTypes from 'prop-types';
-import {useHistory} from 'react-router';
+import {Redirect, useHistory} from 'react-router';
 import Logo from '../logo/logo';
 import {login} from '../../store/api-actions';
 import {connect} from 'react-redux';
-import {AppRoute} from '../../const';
+import {AppRoute, AuthorizationStatus} from '../../const';
+import Loading from '../loading/loading';
 
-const LoginPage = ({onSubmit}) => {
+const LoginPage = ({onSubmit, authorizationStatus}) => {
   const loginRef = useRef();
   const passwordRef = useRef();
   const history = useHistory();
@@ -22,61 +23,70 @@ const LoginPage = ({onSubmit}) => {
   };
 
   return (
-    <div className="user-page">
-      <header className="page-header user-page__head">
-        <Logo/>
+    authorizationStatus === AuthorizationStatus.WAITING_AUTH &&
+    <Loading /> ||
+    authorizationStatus === AuthorizationStatus.AUTH &&
+    <Redirect to={AppRoute.ROOT} /> ||
+    authorizationStatus === AuthorizationStatus.NO_AUTH &&
+      <div className="user-page">
+        <header className="page-header user-page__head">
+          <Logo/>
 
-        <h1 className="page-title user-page__title">Sign in</h1>
-      </header>
+          <h1 className="page-title user-page__title">Sign in</h1>
+        </header>
 
-      <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form" onSubmit={handleSubmit}>
-          <div className="sign-in__fields">
-            <div className="sign-in__field">
-              <input
-                className="sign-in__input"
-                type="email"
-                placeholder="Email address"
-                name="user-email"
-                id="user-email"
-                ref={loginRef}
-              />
-              <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+        <div className="sign-in user-page__content">
+          <form action="#" className="sign-in__form" onSubmit={handleSubmit}>
+            <div className="sign-in__fields">
+              <div className="sign-in__field">
+                <input
+                  className="sign-in__input"
+                  type="email"
+                  placeholder="Email address"
+                  name="user-email"
+                  id="user-email"
+                  ref={loginRef}
+                />
+                <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+              </div>
+              <div className="sign-in__field">
+                <input
+                  className="sign-in__input"
+                  type="password"
+                  placeholder="Password"
+                  name="user-password"
+                  id="user-password"
+                  ref={passwordRef}
+                />
+                <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
+              </div>
             </div>
-            <div className="sign-in__field">
-              <input
-                className="sign-in__input"
-                type="password"
-                placeholder="Password"
-                name="user-password"
-                id="user-password"
-                ref={passwordRef}
-              />
-              <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
+            <div className="sign-in__submit">
+              <button className="sign-in__btn" type="submit">Sign in</button>
             </div>
+          </form>
+        </div>
+
+        <footer className="page-footer">
+          <div className="logo">
+            <a href="main.html" className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
           </div>
-          <div className="sign-in__submit">
-            <button className="sign-in__btn" type="submit">Sign in</button>
+
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
           </div>
-        </form>
+        </footer>
       </div>
-
-      <footer className="page-footer">
-        <div className="logo">
-          <a href="main.html" className="logo__link logo__link--light">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
-    </div>
   );
 };
+
+const mapStateToProps = (state) => ({
+  authorizationStatus: state.authorizationStatus,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   onSubmit(authData) {
@@ -89,4 +99,4 @@ LoginPage.propTypes = {
 };
 
 export {LoginPage};
-export default connect(null, mapDispatchToProps)(LoginPage);
+export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
