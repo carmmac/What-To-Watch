3e2bae8f224a0a330dd38250ee93b4ccf8fc0497
bremxx <petr.kirienko@gmail.com>
2021-03-17 20:00:54 +0300

    рефакторинг логики списка фильмов

--- src/components/films-list/films-list.jsx
+++ src/components/films-list/films-list.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {useSelector} from 'react-redux';
 import {ALL_GENRES, FilmsListLocation} from '../../const';
 import FilmCard from '../film-card/film-card';
@@ -6,41 +7,41 @@ import Loading from '../loading/loading';
 
 const FilmsList = ({filmsVisibleNum, genre, id, location, handleFilmCardClick}) => {
 
-  const {films, favoriteFilms, isLoadedIndicator} = useSelector((state) => state.DATA);
+  const {films, isLoadedIndicator} = useSelector((state) => state.DATA);
 
   if (!isLoadedIndicator.areFilmsLoaded) {
     return <Loading />;
   }
+
+  const getFilmsByLocation = () => {
+    switch (location) {
+      case FilmsListLocation.MAIN:
+        if (genre === ALL_GENRES) {
+          return films.slice(0, filmsVisibleNum);
+        }
+        return films.slice().filter((film) => film.genre === genre).slice(0, filmsVisibleNum);
+
+      case FilmsListLocation.FILM_PAGE:
+        return films.slice().filter((film) => film.genre === genre && film.id !== id);
+
+      default: return [];
+    }
+  };
+
   return (
-    (location === FilmsListLocation.MAIN &&
-      <div className="catalog__movies-list">
-        {
-          (genre === ALL_GENRES &&
-          films.slice(0, filmsVisibleNum)
-              .map((film) => <FilmCard key={film.id} {...film} />))
-          ||
-          films.slice(0, filmsVisibleNum)
-              .filter((film) => film.genre === genre)
-              .map((film) => <FilmCard key={film.id} {...film} />)
-        }
-      </div>)
-    ||
-    (location === FilmsListLocation.FILM_PAGE &&
-      <div className="catalog__movies-list">
-        {
-          films.slice()
-               .filter((film) => film.genre === genre && film.id !== id)
-               .map((film) => <FilmCard handleFilmCardClick={handleFilmCardClick} key={film.id} {...film} />)
-        }
-      </div>)
-    ||
-    (location === FilmsListLocation.USER_LIST &&
-      <div className="catalog__movies-list">
-        {
-          favoriteFilms.slice()
-        }
-      </div>)
+    <div className="catalog__movies-list">
+      {getFilmsByLocation()
+        .map((film) => <FilmCard handleFilmCardClick={handleFilmCardClick} key={film.id} {...film} />)}
+    </div>
   );
+};
+
+FilmsList.propTypes = {
+  id: PropTypes.number,
+  filmsVisibleNum: PropTypes.number,
+  location: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  handleFilmCardClick: PropTypes.func,
 };
 
 export default FilmsList;
