371cfefc7874c49af2336848cc145244607fb521
bremxx <petr.kirienko@gmail.com>
2021-03-23 19:47:54 +0300

    логика добавления фильма в избранное:
    - загрузка избранных фильмов в App;
    - селекторы избранных фильмов и флага их загрузки;
    - рефакторинг блока описания;
    - рефакторинг страницы избранных фильмов;

--- src/components/film-description/film-description.jsx
+++ src/components/film-description/film-description.jsx
@@ -1,38 +1,59 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import PropTypes from 'prop-types';
 import {useDispatch, useSelector} from 'react-redux';
 import {filmPropTypes} from '../../prop-types';
 import {postFilmToFavorites} from '../../store/api-actions';
 import {AuthorizationStatus, DescriptionBlockVersion} from '../../const';
 import {Link} from 'react-router-dom';
+import {makeGetAreFavoriteFilmsLoadedIndicator, makeGetFavoriteFilms} from '../../store/data-reducer/selectors';
+import Loading from '../loading/loading';
 
 const FilmDescription = ({film, version}) => {
 
   const {authorizationStatus} = useSelector((state) => state.USER);
   const dispatch = useDispatch();
 
-  const {id, isFavorite: status} = film;
+  const getAreFavoriteFilmsLoadedIndicator = useMemo(makeGetAreFavoriteFilmsLoadedIndicator, []);
+  const areFavoriteFilmsLoaded = useSelector((state) => getAreFavoriteFilmsLoadedIndicator(state));
+
+  const getFavoriteFilms = useMemo(makeGetFavoriteFilms, []);
+  const favoriteFilms = useSelector((state) => getFavoriteFilms(state));
+
+  const isFilmFavorite = () => {
+    return favoriteFilms.some((favoriteFilm) => favoriteFilm.id === film.id);
+  };
 
   const handleFavoriteClick = () => {
     dispatch(postFilmToFavorites(film.id, film.isFavorite));
   };
 
-  const renderButtonSection = () => {
-    if (authorizationStatus === AuthorizationStatus.AUTH) {
-      return <>
+  if (!areFavoriteFilmsLoaded) {
+    return <Loading />;
+  }
+
+  const renderPrivateButtonSection = () => {
+    return <>
+      {
+        (!isFilmFavorite() &&
         <button className="btn btn--list movie-card__button" type="button" onClick={handleFavoriteClick}>
           <svg viewBox="0 0 19 20" width="19" height="20">
             <use xlinkHref="#add"></use>
           </svg>
           <span>My list</span>
-        </button>
-        {
-          version === DescriptionBlockVersion.FILM_PAGE &&
-          <Link to={`${id}/review`} className="btn movie-card__button">Add review</Link>
-        }
-      </>;
-    }
-    return null;
+        </button>)
+        ||
+        (<button className="btn btn--list movie-card__button" type="button">
+          <svg viewBox="0 0 18 14" width="18" height="14">
+            <use xlinkHref="#in-list"></use>
+          </svg>
+          <span>My list</span>
+        </button>)
+      }
+      {
+        version === DescriptionBlockVersion.FILM_PAGE &&
+        <Link to={`${film.id}/review`} className="btn movie-card__button">Add review</Link>
+      }
+    </>;
   };
 
   return (
@@ -51,7 +72,7 @@ const FilmDescription = ({film, version}) => {
           <span>Play</span>
         </button>
         {
-          renderButtonSection()
+          authorizationStatus === AuthorizationStatus.AUTH && renderPrivateButtonSection()
         }
       </div>
     </div>
