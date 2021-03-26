cb0169d93395093cbc2ca6fe104f3bc2d3b3cdaa
bremxx <petr.kirienko@gmail.com>
2021-03-27 00:12:18 +0300

    функционал таба с комментариями

--- src/components/film-page-reviews/film-page-reviews.jsx
+++ src/components/film-page-reviews/film-page-reviews.jsx
@@ -5,12 +5,26 @@ import {useDispatch, useSelector} from "react-redux";
 import {fetchReviews} from "../../store/api-actions";
 import Loading from "../loading/loading";
 import {getAreReviewsLoadedIndicator, getReviews} from "../../store/data-reducer/selectors";
+import {Link} from "react-router-dom";
+import {AppRoute, AuthorizationStatus} from "../../const";
 
 const FilmPageReviews = ({id}) => {
   const reviews = useSelector((state) => getReviews(state));
   const areReviewsLoaded = useSelector((state) => getAreReviewsLoadedIndicator(state));
 
+  const {authorizationStatus} = useSelector((state) => state.USER);
   const dispatch = useDispatch();
+
+  const renderReviews = () => {
+    if (reviews.length === 0) {
+      return (
+        <Link to={`${id}/review`} style={{color: `#252525`}}>Add the first review</Link>
+      );
+    }
+    return (
+      reviews.map((review, i) => <ReviewItem key={`review-${i}`} review={review} />)
+    );
+  };
   useEffect(() => {
     if (!areReviewsLoaded) {
       dispatch(fetchReviews(id));
@@ -24,8 +38,9 @@ const FilmPageReviews = ({id}) => {
     <div className="movie-card__reviews movie-card__row">
       <ul className="movie-card__reviews-col" style={{width: `100%`, maxWidth: `100%`}}>
         {
-          (reviews.length === 0 && <span style={{color: `#252525`}}>Add the first review</span>) ||
-          (reviews.map((review, i) => <ReviewItem key={`review-${i}`} review={review} />))
+          authorizationStatus === AuthorizationStatus.AUTH && renderReviews()
+          ||
+          <Link to={AppRoute.REVIEW} style={{color: `#252525`}}>Sign in to add the first review</Link>
         }
       </ul>
     </div>
